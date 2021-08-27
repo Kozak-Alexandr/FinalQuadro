46a1de0d7b6c649b4cbd6aed1762ecd6caca1682
Kozak-Alexandr <89406024+Kozak-Alexandr@users.noreply.github.com>
2021-08-28 01:19:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,55 +5,70 @@
 
 
 int main()
-
-
 {
     printf("a*x^2+b*x+c\n"
            "enter a b c: \n" );
 
-    double a = NAN; //не вогнал, когда присваивать, че за NAN//
+    double a = NAN;
     double b = NAN;//live journal деда//sideof.lj.ru//
     double c = NAN;
-
     double x2 = NAN;
     double x1 = NAN;
 
+ scanf("%lg %lg %lg",&a, &b, &c);
 
-    scanf("%lg %lg %lg",&a, &b, &c);
 
-    int numRoots = solve_square(a, b, c, &x1, &x2);
-    switch(numRoots)
-    {
-    case 0:
-        printf("No Solution\n");
-        break;
-    case 1:
-        printf("x= %lg\n", -c/b);
-        printf("haha");
-        break;
-    case 2:
-        printf("x1= %lg\n x2= %lg\n", x1, x2);
-        break;
-    case 4:
-        printf("Any number");
-        break;
-    case 5:
-        printf("x= %lg\n", -b/(2*c));
-        break;
-    case 6:
-        printf("0");
-        break;
-    }
+if  (isnan(a) || isnan(b) || isnan(c))
+{
+    printf(" Did your cat walk on the keyboard?\n");
 }
+else
+    {
+        int numRoots = solve_square(a, b, c, &x1, &x2);
+
+        switch(numRoots)
+        {
+        case 0:
+            printf(" No Solution\n");
+            break;
+        case 1:
+            printf(" x= %lg\n", -c/b);
+            printf(" haha");
+            break;
+        case 2:
+            printf(" x1= %lg\n x2= %lg\n", x1, x2);
+            break;
+        case 4:
+            printf(" Any number");
+            break;
+        case 5:
+            printf(" x= %lg\n", -b/(2*c));
+            break;
+        case 6:
+            printf(" 0");
+            break;
+        }
+    }
+    }
 int cmp_with_0(double l)
 {
     if(l<EPSILON)
+    {
         return 1;
-    else return 0;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int cmp_with_0(double l);
+
+//
+
 int solve_square(double a, double b, double c, double *x1, double *x2)  //isnan isfinite NULL pointer загуглить
+
+//
 {                                                                       // шо такое дефайны
     if(cmp_with_0(a))
     {
@@ -108,4 +123,5 @@ int solve_square(double a, double b, double c, double *x1, double *x2)  //isnan 
 
     return 0;
 }
+
 

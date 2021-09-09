605ddda15a19a03d9c41ed77b3e48e2166a939f3
Kozak-Alexandr <89406024+Kozak-Alexandr@users.noreply.github.com>
2021-09-09 19:11:51 +0300

    Update square_solve.h

--- square_solve.h
+++ square_solve.h
@@ -1,13 +1,15 @@
 #ifndef SQ_H
 #define SQ_H
+#define EPSILON 0.00001
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 void solve_quadratic();
 void solve_discr();
-
+int solve_square();
 
 
 
